fe6e2efb9475b65ce0854679918d7b4a941a483d
Rafael <novaesrafael880@gmail.com>
2025-08-07 10:41:28 -0300

    01- Criando o projeto

---
+++ Condicionais_ex/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Condicionais_ex
+{
+    partial class frmmenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tsmiexs = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiex1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiex2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiex3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiexs});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tsmiexs
+            // 
+            this.tsmiexs.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiex1,
+            this.tsmiex2,
+            this.tsmiex3});
+            this.tsmiexs.Name = "tsmiexs";
+            this.tsmiexs.Size = new System.Drawing.Size(71, 20);
+            this.tsmiexs.Text = "Exercícios";
+            // 
+            // tsmiex1
+            // 
+            this.tsmiex1.Name = "tsmiex1";
+            this.tsmiex1.Size = new System.Drawing.Size(180, 22);
+            this.tsmiex1.Text = "Exercício 1";
+            // 
+            // tsmiex2
+            // 
+            this.tsmiex2.Name = "tsmiex2";
+            this.tsmiex2.Size = new System.Drawing.Size(180, 22);
+            this.tsmiex2.Text = "Exercício 2";
+            // 
+            // tsmiex3
+            // 
+            this.tsmiex3.Name = "tsmiex3";
+            this.tsmiex3.Size = new System.Drawing.Size(180, 22);
+            this.tsmiex3.Text = "Exercício 3";
+            // 
+            // frmmenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmmenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menu";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiexs;
+        private System.Windows.Forms.ToolStripMenuItem tsmiex1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiex2;
+        private System.Windows.Forms.ToolStripMenuItem tsmiex3;
+    }
+}
+
